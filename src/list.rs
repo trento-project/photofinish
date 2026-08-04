1eaba81cb3907ea4223d190054964a4ff752bd79
Antonio Gamez Diaz <antonio.gamez@suse.com>
2026-08-04 20:40:16 +0200

    Add tests
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- src/list.rs
+++ src/list.rs
@@ -17,3 +17,34 @@ fn print_scenario(scenario: &Scenario) {
         scenario.directories.join("\n")
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // println! output isn't captured/asserted here (would need a refactor to inject
+    // a writer); these are smoke tests confirming the functions don't panic.
+
+    #[test]
+    fn show_list_handles_no_scenarios() {
+        show_list(&[]);
+    }
+
+    #[test]
+    fn show_list_handles_scenarios_with_files_and_directories() {
+        show_list(&[Scenario {
+            label: "my-scenario".to_string(),
+            files: vec!["a.json".to_string(), "b.json".to_string()],
+            directories: vec!["some/dir".to_string()],
+        }]);
+    }
+
+    #[test]
+    fn print_scenario_handles_empty_files_and_directories() {
+        print_scenario(&Scenario {
+            label: "empty-scenario".to_string(),
+            files: vec![],
+            directories: vec![],
+        });
+    }
+}
